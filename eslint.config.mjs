576509e059573c5f744fc8d78531642977eaabab
Lorae <10428186+lorae@users.noreply.github.com>
2025-06-21 21:29:06 -0400

    build: implement prettier and configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: 'eslint:recommended',
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends('next', 'next/core-web-vitals'),
+  {
+    languageOptions: {
+      globals: {
+        React: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
+    },
+  },
 ];
-
-export default eslintConfig;
