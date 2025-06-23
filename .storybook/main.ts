6a782000e7408c0f7e3972b4a018df9ca84bb2e6
Lorae <10428186+lorae@users.noreply.github.com>
2025-06-22 22:46:53 -0400

    build: implement storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/types';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+};
+
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import storybook from 'eslint-plugin-storybook';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +13,7 @@ const compat = new FlatCompat({
 
 export default [
   ...compat.extends('next', 'next/core-web-vitals'),
+
   {
     languageOptions: {
       globals: {
@@ -22,4 +24,14 @@ export default [
       'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
     },
   },
+
+  // ✅ Storybook override (flat config style)
+  {
+    files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+    rules: {
+      'storybook/hierarchy-separator': 'error',
+    },
+  },
+
+  ...storybook.configs['flat/recommended'],
 ];
