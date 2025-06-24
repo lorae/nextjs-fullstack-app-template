1f48a79a2c4dd4996695a61ba73541b856449218
Lorae <10428186+lorae@users.noreply.github.com>
2025-06-24 17:33:08 -0400

    feat: build sidebar and primary layouts

---
+++ components/layouts/sidebar/SidebarLayout.mocks.ts
@@ -0,0 +1,14 @@
+import { ISidebarLayout } from './SidebarLayout';
+
+const base: ISidebarLayout = {
+  sampleTextProp: 'Hello world!',
+};
+
+const alt: ISidebarLayout = {
+  sampleTextProp: 'Hello alt world!',
+};
+
+export const mockSidebarLayoutProps = {
+  base,
+  alt,
+};
